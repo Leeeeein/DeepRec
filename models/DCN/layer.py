2050a9b2f1cd7f09a38190164afb59323cedbe36
Leeeeein <704890093@qq.com>
2022-04-03 00:20:06 +0800

    DCN: define dcn model

--- models/DCN/layer.py
+++ models/DCN/layer.py
@@ -40,4 +40,59 @@ class DeepCrossNetwork(nn.Module):
         output = self.output(output_concat)
         return nn.Sigmoid(output)
 
+def get_data():
+    X_train, Y_train, X_test, Y_test, X_val, Y_val = DataLoading(args.train_ratio, args.test_ratio)
+    return X_train, Y_train, X_test, Y_test, X_val, Y_val
+
+def trainer(embed_dim, learning_rate, weight_decay, epochs, batch_size, train_ratio, test_ratio):
+    X_train, Y_train, X_test, Y_test, X_val, Y_val = get_data()
+    train_inputs, train_targets = torch.FloatTensor(X_train), torch.FloatTensor(Y_train)
+    test_inputs, test_targets = torch.FloatTensor(X_test), torch.FloatTensor(Y_test)
+    val_inputs, val_targets = torch.FloatTensor(X_val), torch.FloatTensor(Y_val)
+
+    train_dataset = TensorDataset(train_inputs, train_targets)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+    model = DeepCrossNetwork(train_inputs.shape[1], embed_dim)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    criterion = nn.BCELoss()
+    loss_list = list()
+    for epoch in range(epochs):
+        total_loss = 0
+        for it, (x, y) in enumerate(train_loader):
+            pred = model(x)
+            loss = criterion(pred, y)
+            model.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+            loss_list.append(loss.item())
+            #if it % 30 == 0:
+            print(f'    epochs:[{epoch}], iter:[{it}], average loss:[{total_loss}]')
+            total_loss = 0
+        predict = model(val_inputs)
+        target = val_targets
+        auc = roc_auc_score(target.detach().numpy(), predict.detach().numpy())
+        print(f'epochs:[{epoch}], auc:[{auc}]')
+    plt.plot(range(len(loss_list)), loss_list)
+    plt.show()
+
+if __name__ == '__main__':
+    #print(os.path.dirname(__file__))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--embed_dim', default=128)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    parser.add_argument('--weight_decay', type=float, default=1e-6)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--train_ratio', type=float, default=0.6)
+    parser.add_argument('--test_ratio', type=float, default=0.5)
+    args = parser.parse_args()
+    trainer(args.embed_dim,
+            args.learning_rate,
+            args.weight_decay,
+            args.epochs,
+            args.batch_size,
+            args.train_ratio,
+            args.test_ratio)
 
